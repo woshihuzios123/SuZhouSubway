604bf9c77a40ebf9cfc6117c964ddb0b9d25b95c
woshihuzios123 <woshihuzios123@gmail.com>
2019-08-18 21:53:04 +0800

    feature:增加 Category二级菜单 数据库迁移功能

---
+++ SuZhouSubway.Model/Migrations/20190818135154_AddCategoryFK.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuZhouSubway.Model.Migrations
+{
+    public partial class AddCategoryFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Categories",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_CategoryId",
+                table: "Categories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Categories_CategoryId",
+                table: "Categories",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Categories_CategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_CategoryId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Categories");
+        }
+    }
+}
